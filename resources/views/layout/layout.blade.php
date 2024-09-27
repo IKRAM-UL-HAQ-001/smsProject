09d9a63ae26178e34bf1be49117e039f64a1a2d2
IKRAM-UL-HAQ-001 <mrikramulhaq0005@gmail.com>
2024-09-28 02:53:27 +0500

    password updated function

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -20,7 +20,11 @@
 <script src="{{ asset('vendor/global/global.min.js') }}"></script>
 <script src="{{ asset('js/quixnav-init.js') }}"></script>
 <script src="{{ asset('js/custom.min.js') }}"></script>
-
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
+</script>
 <!-- Datatable -->
 <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('js/plugins-init/datatables.init.js') }}"></script>
